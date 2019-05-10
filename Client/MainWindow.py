9150273d4120952b00c9c76762f49db151cf9e7f
Leshrac-Yan <48577959+Leshrac-Yan@users.noreply.github.com>
2019-05-10 11:49:06 +0800

    Added the icon and title (#3)
    * icon_test
    
    * add the icon and title

--- Client/MainWindow.py
+++ Client/MainWindow.py
@@ -1,5 +1,6 @@
 import Login
 from PyQt5 import QtWidgets
+from PyQt5.QtGui import QIcon
 
 class MainWindow(QtWidgets.QMainWindow):
     
@@ -10,3 +11,6 @@ class MainWindow(QtWidgets.QMainWindow):
     def initUI(self):
         self.login=Login.Login()
         self.setCentralWidget(self.login)
+        self.icon=QIcon('loginIcon(Temporary).jpg')
+        self.setWindowIcon(self.icon)
+        self.setWindowTitle('Login')
