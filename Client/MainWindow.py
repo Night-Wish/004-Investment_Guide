d711d12482287032221f6ba3c652b927522545b4
Percy <1772761454@qq.com>
2019-05-11 13:55:48 +0800

    Added a slot which is connected to Login's signal. It is responsible for changing between Login and StockBrowser

--- Client/MainWindow.py
+++ Client/MainWindow.py
@@ -8,6 +8,7 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self,parent=None):
         QtWidgets.QMainWindow.__init__(self,parent)
         self.initUI()
+        self.setupConnection()
         
     def initUI(self):
         self.login=Login.Login()
@@ -15,3 +16,13 @@ class MainWindow(QtWidgets.QMainWindow):
         self.icon=QIcon('loginIcon(Temporary).jpg')
         self.setWindowIcon(self.icon)
         self.setWindowTitle('Login')
+        
+    def setupConnection(self):
+        self.login.serverFeedback.connect(self.feedbackReceived)
+        
+    def feedbackReceived(self,feedback):
+        if feedback == '1':
+            self.stockBrowser=StockBrowser.StockBrowser()
+            self.login.close()
+            self.setCentralWidget(self.stockBrowser)
+            self.resize(800,400)
