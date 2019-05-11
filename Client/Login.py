cfa496a3302993e0c115dbf95ebf1e0270c199af
Percy <1772761454@qq.com>
2019-05-11 13:54:09 +0800

    Did 2 things: 1. Split initUI() into initUI() and arrangeUI(); 2. Added a signal which is emitted when loginBtn is clicked

--- Client/Login.py
+++ Client/Login.py
@@ -1,10 +1,14 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets,QtCore
 
 class Login(QtWidgets.QWidget):
-
+    
+    serverFeedback=QtCore.pyqtSignal(str)
+    
     def __init__(self,parent=None):
         QtWidgets.QWidget.__init__(self,parent)
         self.initUI()
+        self.arrangeUI()
+        self.setupConnection()
         
     def initUI(self):
         self.usernameLabel=QtWidgets.QLabel('Username:')
@@ -16,6 +20,10 @@ class Login(QtWidgets.QWidget):
         self.loginPushBtn=QtWidgets.QPushButton('Login')
         
         self.mainLayout=QtWidgets.QGridLayout(self)
+        
+        self.setWindowTitle("Login")
+        
+    def arrangeUI(self):
         self.mainLayout.addWidget(self.usernameLabel,0,0)
         self.mainLayout.addWidget(self.usernameLineEdit,0,1,1,2)
         self.mainLayout.addWidget(self.passwordLabel,1,0)
@@ -24,4 +32,8 @@ class Login(QtWidgets.QWidget):
         self.mainLayout.addWidget(self.autoLogCheckBox,2,1)
         self.mainLayout.addWidget(self.loginPushBtn,2,2)
         
-        self.setWindowTitle("Login")
+    def setupConnection(self):
+        self.loginPushBtn.clicked.connect(self.loginBtnClicked)
+        
+    def loginBtnClicked(self):
+        self.serverFeedback.emit('1')
