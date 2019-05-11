b05503b1db0ce733a45c91942727418873ff169c
Percy <1772761454@qq.com>
2019-05-11 20:01:04 +0800

    Finished search function

--- Client/StockBrowser.py
+++ Client/StockBrowser.py
@@ -10,8 +10,7 @@ class StockBrowser(QtWidgets.QWidget):
     def __init__(self,parent=None):
         QtWidgets.QWidget.__init__(self,parent)
         self.initUI()
-        self.arrangeUI()
-        self.fillStockList()
+        self.setupConnection()
     
     def initUI(self):
         self.searchLineEdit=SearchLineEdit.SearchLineEdit()
@@ -19,21 +18,19 @@ class StockBrowser(QtWidgets.QWidget):
         self.stockPriceGraph=self.plotRandomStock()
         
         self.leftLayout=QtWidgets.QVBoxLayout()
-        
-        self.middleLayout=QtWidgets.QVBoxLayout()
-        
-        self.mainLayout=QtWidgets.QHBoxLayout(self)
-        
-    def arrangeUI(self):
         self.leftLayout.addWidget(self.searchLineEdit)
         self.leftLayout.addWidget(self.stockList)
         
+        self.middleLayout=QtWidgets.QVBoxLayout()
         self.middleLayout.addWidget(self.stockPriceGraph)
         
+        self.mainLayout=QtWidgets.QHBoxLayout(self)
         self.mainLayout.addLayout(self.leftLayout)
         self.mainLayout.addLayout(self.middleLayout)
         self.mainLayout.setStretchFactor(self.leftLayout,3)
         self.mainLayout.setStretchFactor(self.middleLayout,7)
+        
+        self.fillStockList()
         
     def plotRandomStock(self):
         self.stockCode='002230'
@@ -48,6 +45,32 @@ class StockBrowser(QtWidgets.QWidget):
         return canvas
     
     def fillStockList(self):
-        stockList=np.loadtxt('industry.txt',str,delimiter=',')
-        for stock in stockList:
+        self.rawData=np.loadtxt('industry.txt',str,delimiter=',')
+        for stock in self.rawData:
             self.stockList.addItem(stock[0]+'\t'+stock[1]+'\t'+stock[2])
+            
+    def setupConnection(self):
+        self.searchLineEdit.textChanged.connect(self.searchStockList)
+        
+    def searchStockList(self):
+        searchContent=self.searchLineEdit.text()
+        matchRateList=self.matchRate(searchContent)
+        matchRateList.sort(key=self.getMatchRate,reverse=True)
+        self.stockList.clear()
+        for stock in matchRateList:
+            info=stock[1]
+            self.stockList.addItem(info[0]+'\t'+info[1]+'\t'+info[2])
+        
+    def matchRate(self,searchContent):
+        matchRateList=[]
+        for stock in self.rawData:
+            count=0
+            for info in stock:
+                if searchContent in info:
+                    count+=len(searchContent)
+            matchRateList.append([str(count),stock])
+        return matchRateList
+        
+    def getMatchRate(self,matchRateList):
+        return matchRateList[0]
+                    
