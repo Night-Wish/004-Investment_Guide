fa1c797f826573669c9f0f52187108d420979c72
Percy <1772761454@qq.com>
2019-05-12 09:24:11 +0800

    Changed plotting from matplotlib to pyqtgraph

--- Client/StockBrowser.py
+++ Client/StockBrowser.py
@@ -1,6 +1,11 @@
-from PyQt5 import QtWidgets
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+'''
+在Anaconda Prompt中输入pip install pyecharts==0.5.6
+更高级的版本无法使用
+该程序一定会在第二次运行时崩溃，初步诊断为QWebEngineView()的问题
+'''
+
+from PyQt5 import QtWidgets,QtCore
+import pyqtgraph as pg
 import tushare as ts
 import numpy as np
 import SearchLineEdit
@@ -15,14 +20,14 @@ class StockBrowser(QtWidgets.QWidget):
     def initUI(self):
         self.searchLineEdit=SearchLineEdit.SearchLineEdit()
         self.stockList=QtWidgets.QListWidget()
-        self.stockPriceGraph=self.plotRandomStock()
+        self.stockGraph=pg.PlotWidget(self)
         
         self.leftLayout=QtWidgets.QVBoxLayout()
         self.leftLayout.addWidget(self.searchLineEdit)
         self.leftLayout.addWidget(self.stockList)
         
         self.middleLayout=QtWidgets.QVBoxLayout()
-        self.middleLayout.addWidget(self.stockPriceGraph)
+        self.middleLayout.addWidget(self.stockGraph)
         
         self.mainLayout=QtWidgets.QHBoxLayout(self)
         self.mainLayout.addLayout(self.leftLayout)
@@ -31,23 +36,20 @@ class StockBrowser(QtWidgets.QWidget):
         self.mainLayout.setStretchFactor(self.middleLayout,7)
         
         self.fillStockList()
+        self.plotStockGraph()
         
-    def plotRandomStock(self):
-        self.stockCode='002230'
-        data=ts.get_hist_data(self.stockCode)
-        data=data['close']
-        data=np.array(data)
-        data=data[::-1]
-        figure=plt.figure()
-        plt.plot(data)
-        canvas=FigureCanvas(figure)
-        canvas.draw()
-        return canvas
     
     def fillStockList(self):
-        self.rawData=np.loadtxt('industry.txt',str,delimiter=',')
-        for stock in self.rawData:
+        self.industryData=np.loadtxt('industry.txt',str,delimiter=',')
+        for stock in self.industryData:
             self.stockList.addItem(stock[0]+'\t'+stock[1]+'\t'+stock[2])
+            
+    def plotStockGraph(self):
+        firstStock=self.industryData[0]
+        data=ts.get_hist_data(firstStock[0],ktype='5')
+        data=data['close']
+        data=np.array(data)
+        self.stockGraph.plot(data)
             
     def setupConnection(self):
         self.searchLineEdit.textChanged.connect(self.searchStockList)
@@ -63,7 +65,7 @@ class StockBrowser(QtWidgets.QWidget):
         
     def matchRate(self,searchContent):
         matchRateList=[]
-        for stock in self.rawData:
+        for stock in self.industryData:
             count=0
             for info in stock:
                 if searchContent in info:
